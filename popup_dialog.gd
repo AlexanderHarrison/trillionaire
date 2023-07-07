c173a2da27dc9ddc1a7f2f3da8f3fd98e388e49e
RoscoeSmith <roscoe1@ualberta.ca>
2023-07-07 17:20:49 -0600

    more attempts

--- popup_dialog.gd
+++ popup_dialog.gd
@@ -3,5 +3,5 @@ extends CenterContainer
 
 func init(text: String):
 	$AcceptDialog.set_text(text)
-	$AcceptDialog.position = self.get_screen_position()
+	#$AcceptDialog.position = self.get_screen_position()
 	$AcceptDialog.visible = true
