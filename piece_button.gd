5ab1da9927ee0c519d70c1da841c46f57fd369ac
Alex Harrison <alexander.harrison.r@gmail.com>
2023-07-07 17:02:32 -0600

    mouse events on piece list

--- piece_button.gd
+++ piece_button.gd
@@ -1,7 +1,21 @@
 extends Container
 
 @export var piece_name: String
+var mouseover = false;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$CenterContainer/RichTextLabel.set_text(piece_name);
+
+func _on_mouse_entered():
+	mouseover = true
+
+func _on_mouse_exited():
+	mouseover = false
+
+func _on_gui_input(event):
+	if event is InputEventMouseButton \
+		and event.button_index == MOUSE_BUTTON_LEFT \
+		and event.is_pressed() \
+		and mouseover:
+		print(piece_name)
