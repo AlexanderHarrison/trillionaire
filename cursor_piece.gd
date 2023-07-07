e004b82ca094e36053e78b6b542ce1498ee7c81e
Alex Harrison <alexander.harrison.r@gmail.com>
2023-07-07 17:32:03 -0600

    fixes

--- cursor_piece.gd
+++ cursor_piece.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
-func _process(delta):
+func _process(_delta):
 	var mouse_position = get_viewport().get_mouse_position()
 	global_position = mouse_position
+	
+func start():
+	self.visible = true
+
+func end():
+	self.visible = false
