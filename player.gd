9f584154213edaecd4cc10d357ff7fd697751605
RoscoeSmith <roscoe1@ualberta.ca>
2023-07-07 21:44:29 -0600

    Added some player information

--- player.gd
+++ player.gd
@@ -3,9 +3,17 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.money = 10000  # Encoded as cents
+	self.money = 100000000000000  # Encoded as cents
+	self.assets = []
+	self.portfolio = []
+	self.company_expenses = 50000
+	self.company_earnings = 60000
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+#Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func get_profit() -> int:
+	return self.company_earnings - self.company_expenses
