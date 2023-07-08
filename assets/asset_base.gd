ad25994edbb847996ab10bffa1063f68aa802212
RoscoeSmith <roscoe1@ualberta.ca>
2023-07-07 22:28:24 -0600

    Created asset base class

---
+++ assets/asset_base.gd
@@ -0,0 +1,15 @@
+extends Node
+
+func init(name: String, desc: String, rng_chance: float):
+	self.name = name
+	self.description = desc
+	self.random_chance = rng_chance
+
+func daily(player):
+	pass
+	
+func weekly(player):
+	pass
+	
+func random(player):
+	pass
