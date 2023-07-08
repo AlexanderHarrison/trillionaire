5ea8a7a20dcb5518ba082a38725fe346b491fcc5
Alex Harrison <alexander.harrison.r@gmail.com>
2023-07-07 20:01:46 -0600

    I am exhausted

--- free_hour.gd
+++ free_hour.gd
@@ -4,9 +4,13 @@ var mouseover = false;
 var parent;
 @export var idx: int;
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _ready():
 	parent = get_parent()
+	if parent.add_child_hour != null:
+		parent.add_child_hour(self);
+
+func set_full():
+	self.color = "FFFFFFFF"
 
 func _on_mouse_entered():
 	mouseover = true
@@ -14,11 +18,9 @@ func _on_mouse_entered():
 func _on_mouse_exited():
 	mouseover = false
 
-
 func _on_gui_input(event):
 	if event is InputEventMouseButton \
 		and event.button_index == MOUSE_BUTTON_LEFT \
 		and event.is_pressed() \
 		and mouseover:
 		parent.box_click(idx)
-		
--- schedule.gd
+++ schedule.gd
@@ -1,13 +1,68 @@
 extends GridContainer
 
+var cursorpiece
+
 var calendar = [
-	[null, null, null, null, null],
-	[null, null, null, null, null],
-	[null, null, null, null, null],
-	[null, null, null, null, null],
-	[null, null, null, null, null],
-	[null, null, null, null, null]
+	[false, false, false, false, false],
+	[false, false, false, false, false],
+	[false, false, false, false, false],
+	[false, false, false, false, false],
+	[false, false, false, false, false],
+	[false, false, false, false, false],
 ];
 
+var width = 5
+var height = 6
+
+var arrangements = []
+var hour_nodes = [
+	[null, null, null, null, null], 
+	[null, null, null, null, null], 
+	[null, null, null, null, null], 
+	[null, null, null, null, null], 
+	[null, null, null, null, null],
+	[null, null, null, null, null]
+]
+
+func _ready():
+	cursorpiece = get_tree().root.get_child(0).get_node("cursor_piece")
+
+func add_child_hour(hour):
+	var here_x: int = hour.idx % 5
+	var here_y: int = floori(hour.idx / 5)
+	hour_nodes[here_y][here_x] = hour
+
 func box_click(idx):
-	print(idx)
+	if cursorpiece.has_cursor():
+		var piece = cursorpiece.get_piece()
+		if valid_arrangement(idx, piece.arrangement()):
+			add_arrangement(idx, piece.arrangement())
+		else:
+			pass
+		cursorpiece.clear_cursor()
+
+func valid_arrangement(idx, arrangement):
+	var here_x: int = idx % 5
+	var here_y: int = floori(idx / 5)
+	
+	for y in arrangement.size():
+		for x in arrangement[0].size():
+			if arrangement[y][x]:
+				if here_x + x >= width:
+					return false
+				if here_y + y >= height:
+					return false
+				if calendar[here_y+y][here_x+x]:
+					return false
+	return true
+			
+func add_arrangement(idx, arrangement):
+	var here_x: int = idx % 5
+	var here_y: int = floori(idx / 5)
+	
+	arrangements.append(arrangement)
+	for y in arrangement.size():
+		for x in arrangement[0].size():
+			if arrangement[y][x]:
+				calendar[here_y+y][here_x+x] = true
+				hour_nodes[here_y+y][here_x+x].set_full()
